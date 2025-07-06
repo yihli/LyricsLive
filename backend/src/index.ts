8f7d4aee80c36109ecb6d5ce7ebf027e13138e95
yihli <154848136+yihli@users.noreply.github.com>
2025-07-06 15:18:22 -0700

    set up redis

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,22 @@ import app from './app';
 
 import { PORT } from './utils/env_setup';
 
+// REDIS
+
+import { createClient } from 'redis';
+
+const client = createClient({
+    url: 'redis://redis:6379'
+});
+
+client.on('error', err => console.log('Redis Client Error', err));
+
+client.connect().then(() => {
+    console.log('Connected to redis');
+});
+
+// REDIS
+
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
