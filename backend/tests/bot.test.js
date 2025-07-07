23e8ca4bc11e6b1ed90926b8c945c6e1695cd7ab
yihli <154848136+yihli@users.noreply.github.com>
2025-07-07 14:19:40 -0700

    add test for discord js startup

---
+++ backend/tests/bot.test.js
@@ -0,0 +1,29 @@
+import app from '../src/app';
+import discordBot from '../src/discord/bot';
+import supertest from 'supertest';
+
+import { afterAll, describe, expect, it } from '@jest/globals';
+
+import { DISCORD_TOKEN } from '../src/utils/env_setup';
+
+describe('startup', () => {
+    afterEach(async () => {
+        await discordBot.destroy();
+    });
+
+    it('successfully connects to the discord bot', async () => {
+        let success = false;
+
+        await discordBot.login(DISCORD_TOKEN);
+        // promise to prevent race conditions
+        await new Promise((resolve) => {
+            discordBot.once('ready', () => {
+                success = true;
+                console.log('success');
+                resolve();
+            });
+        });
+
+        expect(success).toBe(true);
+    })
+})
